0ac7a2be65f1187121132f9ea1aa1e720aa97402
s58DB <s58DB@users.noreply.github.com>
2026-05-09 23:23:11 +0200

    Revert "Treat reflector stop as inactive"
    This reverts commit 9374b28e0d7a3775ea71ee3f123f56c192b42772.

--- include/functions.php
+++ include/functions.php
@@ -370,7 +370,7 @@ function getHeardList($logLines) {
                 $target = "TG ".trim(get_string_between($logLine, "#", ":"));
 		$source = "SVXRef";
 		$timestamp = getSVXTimestampFromLogLine($logLine);
-                $tx = "OFF";
+                $tx = isRecentSVXStopTimestamp($timestamp) ? "ON" : "OFF";
                } 
 		if ($isTalkerStart) {
                  $calltemp = substr($logLine,strpos($logLine,"TG")+4,27);
