b43a75493ba7d13e35f06f489bd8c13aae68f6d0
s58DB <s58DB@users.noreply.github.com>
2026-05-09 16:11:17 +0200

    Replace header logo

--- dtmf.php
+++ dtmf.php
@@ -47,7 +47,7 @@ include_once "include/tools.php";
 <div class="container"> 
 <div class="header">
 <div class="site-header-inner">
-    <div class="site-logo"><img src="images/svxlink.ico" alt="SVXLink" /></div>
+    <div class="site-logo"><img src="images/svxlinks5_logo.png" alt="SVXLink" /></div>
     <div class="site-title">
 <?php
 $svxConfigFile = '/etc/svxlink/svxlink.conf';
