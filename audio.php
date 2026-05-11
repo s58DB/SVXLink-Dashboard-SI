5ae485a2d29d0ebb2d155a80049964c956a6794a
s58DB <s58DB@users.noreply.github.com>
2026-05-11 20:51:10 +0200

    Modernize audio test dashboard wrapper

--- audio.php
+++ audio.php
@@ -28,6 +28,7 @@ include_once "include/tools.php";
     <meta http-equiv="expires" content="0" />
     <meta http-equiv="pragma" content="no-cache" />
 <link rel="shortcut icon" href="images/favicon.ico" sizes="16x16 32x32" type="image/png">
+    <link href="css/css.php" type="text/css" rel="stylesheet" />
  
 <?php echo ("<title>" . $callsign ." SI v0.1 Dashboard</title>"); ?>
 
@@ -42,27 +43,19 @@ include_once "include/tools.php";
     <script src="scripts/featherlight.js" type="text/javascript" charset="utf-8"></script>
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
-<body style = "background-color: #e1e1e1;font: 11pt arial, sans-serif;">
+<body style = "font: 11pt arial, sans-serif;">
 <center>
-<fieldset style = "box-shadow:5px 5px 20px #999; background-color:#f1f1f1; width:900px;margin-top:15px;margin-left:0px;margin-right:5px;font-size:13px;border-top-left-radius: 10px; border-top-right-radius: 10px;border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
+<fieldset style = "box-shadow:0 10px 28px rgba(11,47,99,0.16); background-color:#ffffff; width:900px;margin-top:15px;margin-left:0px;margin-right:5px;font-size:13px;border:1px solid #d7e0ea;border-radius:8px;">
 <div class="container"> 
 <div class="header">
-<div class="parent">
-    <div class="img" style = "padding-left:30px"><img src="images/svxlinks5_logo_no_background.png" /></div>
-    <div class="text"style = "padding-right:230px">
-<center><p style = "margin-top:5px;margin-bottom:0px;">
-<?php
-$svxConfigFile = '/etc/svxlink/svxlink.conf';
-    if (fopen($svxConfigFile,'r')) 
-       { $svxconfig = parse_ini_file($svxConfigFile,true,INI_SCANNER_RAW); 
-         $callsign = $svxconfig['ReflectorLogic']['CALLSIGN'];}
-    else { $callsign="NOCALL";}
-?>
-<span style = "font-size: 32px;letter-spacing:4px;font-family: &quot;sans-serif&quot;, sans-serif;font-weight:500;color:#fff8dc"><?php echo $callsign; ?></span>
-<p style = "margin-top:0px;margin-bottom:0px;">
-<span style = "font-size: 18px;letter-spacing:4px;font-family: &quot;sans-serif&quot;, sans-serif;font-weight:500;color:#fff8dc"><?php echo $fmnetwork; ?></span>
-</p></center>
-</div></div>
+<div class="site-header-inner">
+    <div class="site-logo"><img src="images/svxlinks5_logo_no_background.png" alt="SVXLink" /></div>
+    <div class="site-title">
+        <span class="callsign"><?php echo $callsign; ?></span>
+        <span class="network"><?php echo $fmnetwork; ?></span>
+    </div>
+    <div class="site-emblem"><strong>ZRS</strong>Zveza radioamaterjev Slovenije</div>
+</div>
 </div>
 
 <?php include_once "include/top_menu.php"; ?>
@@ -100,7 +93,7 @@ include_once "include/buttons.php";
 
     echo '<td valign="center" style = "height:auto;border:none;  background-color:#f1f1f1;">';
 
-    echo '<iframe src="/audio"  width="600" height="500" title="audio"></iframe>';
+    echo '<iframe src="/audio/" width="620" height="560" title="SVXLink Audio Test Peak Meter" style="border:0;"></iframe>';
 
 
 
